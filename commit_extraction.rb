493a26721f02731fe1ee7576c4ca46551e2c3296
Allan Jones <allanpauljones@gmail.com>
2014-09-19 11:26:34 +1000

    QUALITY: Removed unnecessary whitespace

--- commit_extraction.rb
+++ commit_extraction.rb
@@ -2,7 +2,7 @@
 #  Create by: Mary Gomez
 #  Date: 29/08/2014
 #  Modified by:
-#  Date: 
+#  Date:
 #  Description: Script for get data from Bitbucket and GitHub
 #               (Commit's Messages)
 # //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
@@ -48,8 +48,8 @@ end
   config.adapter       = :net_http
 end
 
-@bitbucket_consumer = OAuth::Consumer.new ENV["COMMIT_EXTRACTION_BITBUCKET_CLIENT_ID"], 
-                                ENV["COMMIT_EXTRACTION_BITBUCKET_CLIENT_SECRET"], 
+@bitbucket_consumer = OAuth::Consumer.new ENV["COMMIT_EXTRACTION_BITBUCKET_CLIENT_ID"],
+                                ENV["COMMIT_EXTRACTION_BITBUCKET_CLIENT_SECRET"],
                                 {:site=>"https://bitbucket.org"}
 
 @github = Octokit::Client.new(:access_token => ENV["COMMIT_EXTRACTION_GITHUB_ACCESS_TOKEN"])
@@ -60,7 +60,7 @@ end
 
 @github_organization        = ENV["COMMIT_EXTRACTION_ORGANIZATION"]
 @github_commits_limit       = 100
-@bitbucket_commits_limit    = 50 
+@bitbucket_commits_limit    = 50
 @array_repositories_github  = Array.new
 
 FileUtils.mkdir('output') unless Dir.exists?('output')
@@ -79,7 +79,7 @@ end
 # //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 
 def category(message)
-  case message[0,10].upcase 
+  case message[0,10].upcase
     when /FIX/
       category_message = "FIXED"
     when /NEW/, /ADD/, /IMPLEMENT/
@@ -116,23 +116,23 @@ end
 
 def bitbucket_commits_count(repo_owner, repo_slug)
   begin
-    commits = @bitbucket.repos.changesets.list repo_owner, repo_slug, :limit => '1' 
+    commits = @bitbucket.repos.changesets.list repo_owner, repo_slug, :limit => '1'
     commits_count = commits[:count]
     return commits_count
-  rescue 
+  rescue
     return 0
   end
 end
 
 def bitbucket_commits_by_page(num_page, repo_owner, repo_slug, start_commit_node)
   if num_page == 1
-    commits = @bitbucket.repos.changesets.list repo_owner, repo_slug, 
+    commits = @bitbucket.repos.changesets.list repo_owner, repo_slug,
                                                  :limit => @bitbucket_commits_limit
   else
-    commits = @bitbucket.repos.changesets.list repo_owner, repo_slug, 
-                                                 :limit => @bitbucket_commits_limit, 
+    commits = @bitbucket.repos.changesets.list repo_owner, repo_slug,
+                                                 :limit => @bitbucket_commits_limit,
                                                  :start => start_commit_node
-  end 
+  end
   return commits
 end
 
@@ -151,18 +151,18 @@ def bitbucket_commits_details(repo_owner, repo_slug, commits, is_last_page)
   CSV.open(@filename, "ab") do |csv|
     commits.changesets.each  do |change|
       change.files.each do |file|
-        if num_node == 1 
-          start_commit_node = change.node 
+        if num_node == 1
+          start_commit_node = change.node
         end
         if start_commit_node != change.node || is_last_page
-          commit_repository = repo_slug             
-          commit_branche    = change.branche     
-          commit_sha        = change.raw_node       
-          commit_fileName   = file.file             
-          commit_author     = change.author         
-          commit_message    = change.message        
+          commit_repository = repo_slug
+          commit_branche    = change.branche
+          commit_sha        = change.raw_node
+          commit_fileName   = file.file
+          commit_author     = change.author
+          commit_message    = change.message
           commit_category   = category(commit_message)
-          commit_date       = change.timestamp.to_s    
+          commit_date       = change.timestamp.to_s
           response = @bitbucket_consumer.request(:get, "https://bitbucket.org/api/1.0/repositories/#{repo_owner}/#{repo_slug}/changesets/#{change.raw_node}/diffstat/")
           hash = JSON.parse(response.body)
           for i in 0..hash.size - 1
@@ -175,12 +175,12 @@ def bitbucket_commits_details(repo_owner, repo_slug, commits, is_last_page)
                 @commit_additions = item[1]
               end
                 @commit_changes   = @commit_deletions.to_i + @commit_additions.to_i
-            end     
-            csv << ["Bitbucket", commit_repository, commit_branche, commit_sha, commit_fileName, @commit_deletions, @commit_additions, @commit_changes, commit_author, commit_category.to_s, commit_message, commit_date] 
+            end
+            csv << ["Bitbucket", commit_repository, commit_branche, commit_sha, commit_fileName, @commit_deletions, @commit_additions, @commit_changes, commit_author, commit_category.to_s, commit_message, commit_date]
           end
         end
         num_node = num_node + 1
-      end 
+      end
     end
   end
   return start_commit_node
@@ -204,7 +204,7 @@ def bitbucket_repositories
       next
     end
     bitbucket_commits(repo.owner, repo.slug)
-  end 
+  end
 end
 
 # //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
@@ -215,32 +215,32 @@ def github_commits_details(repo_full_name, branche,  sha)
   CSV.open(@filename, "ab") do |csv|
     commit = @github.commit repo_full_name, sha
     commit[:files].each do |item|
-      commit_repository = repo_full_name                    
-      commit_branche    = branche            
-      commit_sha        = commit.sha                   
-      commit_fileName   = item[:filename]  
-      commit_additions  = item[:additions].to_s         
-      commit_deletions  = item[:deletions].to_s         
-      commit_changes    = item[:changes].to_s               
-      commit_author     = commit.commit.author.name     
-      commit_message    = commit.commit.message          
+      commit_repository = repo_full_name
+      commit_branche    = branche
+      commit_sha        = commit.sha
+      commit_fileName   = item[:filename]
+      commit_additions  = item[:additions].to_s
+      commit_deletions  = item[:deletions].to_s
+      commit_changes    = item[:changes].to_s
+      commit_author     = commit.commit.author.name
+      commit_message    = commit.commit.message
       commit_category   = category(commit_message)
-      commit_date       = commit.commit.author.date.to_s 
-      csv << ["GitHub",commit_repository, commit_branche, commit_sha, commit_fileName, commit_deletions, commit_additions, commit_changes, commit_author, commit_category.to_s, commit_message, commit_date] 
+      commit_date       = commit.commit.author.date.to_s
+      csv << ["GitHub",commit_repository, commit_branche, commit_sha, commit_fileName, commit_deletions, commit_additions, commit_changes, commit_author, commit_category.to_s, commit_message, commit_date]
     end
   end
 end
 
 
 def github_commits_by_page(repo_full_name, branche)
-  start_commit_sha = '' 
+  start_commit_sha = ''
   num_page = 1
   loop do
     commits = @github.commits repo_full_name, branche, { page: num_page, per_page: @github_commits_limit }
     commits_size = commits.size
     for i in 0..commits.size - 1
       github_commits_details(repo_full_name, branche, commits[i][:sha])
-    end 
+    end
     break if commits_size != 0
   end
 end
@@ -249,15 +249,15 @@ def github_commits_branches(repo_full_name)
   branches = @github.branches repo_full_name
   for i in 0..branches.size - 1
      github_commits_by_page(repo_full_name, branches[i][:name])
-  end 
+  end
 end
 
 def github_repositories
   x = 0
-  repos = @github.organization_repositories @github_organization, { type: 'sources' } 
+  repos = @github.organization_repositories @github_organization, { type: 'sources' }
   repos.each do |repo|
     @array_repositories_github[x] = repo.name
-    x += 1 
+    x += 1
     github_commits_branches(repo.full_name)
   end
 end
@@ -291,12 +291,3 @@ end
 
 current_time = Time.now
 puts "End " + current_time.strftime("%Y-%m-%d %H:%M:%S")
-
-
-
-  
-    
-   
-
-
-
